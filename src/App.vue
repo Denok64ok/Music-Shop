d004ea4172019b9e74f606de886b41c9f11d941f
denok64ok <yaden9483@gmail.com>
2024-11-09 14:20:22 +0500

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
+    <Header />
     <router-view />
   </div>
 </template>
 
-<script></script>
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <div class="logo">
+      <img :src="require('@/assets/logo.png')" alt="Music Store Logo" />
+      <h1>Music Store</h1>
+    </div>
+    <nav class="nav">
+      <router-link to="/" class="nav-link">Главная</router-link>
+      <router-link to="/catalog" class="nav-link">Каталог</router-link>
+      <router-link to="/about" class="nav-link">О нас</router-link>
+    </nav>
+    <div class="icons">
+      <router-link to="/cart" class="icon-link">
+        <img :src="require('@/assets/images/cart-icon.svg')" alt="Корзина" />
+      </router-link>
+      <router-link to="/profile" class="icon-link">
+        <img :src="require('@/assets/images/user-icon.svg')" alt="Профиль" />
+      </router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped></style>
